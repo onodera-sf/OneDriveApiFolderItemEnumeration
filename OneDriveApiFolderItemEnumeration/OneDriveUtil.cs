2faf4cba2bcaaedfaf1ae33ca61ae69d46b71c45
onodera-sf <sf_yo_interactive@outlook.jp>
2025-01-04 12:37:50 +0900

    プロジェクト ファイルを追加します。

---
+++ OneDriveApiFolderItemEnumeration/OneDriveUtil.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Models;
+
+public static class OneDriveUtil
+{
+	/// <summary>
+	/// フォルダを作成します。
+	/// すでにフォルダがある場合は何もしません。
+	/// </summary>
+	/// <param name="graphClient">GraphServiceClient。</param>
+	/// <param name="oneDriveId">OneDrive の ID。フォルダの ID ではありません。</param>
+	/// <param name="folderId">フォルダを作成する親フォルダの ID。</param>
+	/// <param name="folderName">作成するフォルダ名。</param>
+	/// <returns>作成したフォルダの情報。</returns>
+	public async static Task<DriveItem?> CreateFolder(GraphServiceClient graphClient, string oneDriveId, string folderId, string folderName)
+	{
+		var requestBody = new DriveItem
+		{
+			Name = folderName,
+			Folder = new(),
+			AdditionalData = new Dictionary<string, object>
+			{
+				{"@microsoft.graph.conflictBehavior" , "replace" },
+      },
+		};
+		return await graphClient.Drives[oneDriveId].Items[folderId].Children.PostAsync(requestBody);
+	}
+
+	/// <summary>
+	/// ファイルをアップロードします。上限はおおよそ 50MB 付近です。
+	/// </summary>
+	/// <param name="graphClient">GraphServiceClient。</param>
+	/// <param name="oneDriveId">OneDrive の ID。フォルダの ID ではありません。</param>
+	/// <param name="uploadFolderId">アップロード先フォルダ ID。</param>
+	/// <param name="fileName">アップロード先のファイル名。</param>
+	/// <param name="uploadData">アップロードするデータ。</param>
+	/// <returns></returns>
+	public async static Task<DriveItem?> UploadFile(GraphServiceClient graphClient, string oneDriveId, string uploadFolderId, string fileName, Stream uploadData)
+		=> await graphClient.Drives[oneDriveId].Items[uploadFolderId].ItemWithPath(fileName).Content.PutAsync(uploadData);
+}
---
+++ OneDriveApiFolderItemEnumeration/Program.cs
@@ -0,0 +1,92 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+
+// 各種 ID などの定義
+var clientId = "XXXXXXXX";      // クライアント ID
+var tenantId = "XXXXXXXX";      // テナント ID
+var clientSecret = "XXXXXXXX";  // クライアント シークレット
+var userId = "XXXXXXXX";        // ユーザー ID
+
+var testFolderName = "TestFolder";
+var isCreateTestFile = true;     // テスト用に200件以上のファイルを作成するかどうか。そこそこ時間が掛かるので注意
+
+
+// 使いまわすので最初に定義しておく
+HttpClient httpClient = new();
+
+// クライアント シークレットによる認証情報を定義
+ClientSecretCredential clientSecretCredential = new(tenantId, clientId, clientSecret);
+
+// HttpClient と認証情報で Microsoft Graph サービスのクライアントを生成
+using GraphServiceClient graphClient = new(httpClient, clientSecretCredential);
+
+// 対象ユーザーに紐づく OneDrive を取得 (紐づいているドライブが OneDrive １つという前提)
+var drive = await graphClient.Users[userId].Drive.GetAsync();
+if (drive == null || drive.Id == null)
+{
+	Console.WriteLine("ドライブを取得できませんでした。");
+	return;
+}
+
+// OneDrive のルートを取得
+var root = await graphClient.Drives[drive.Id].Root.GetAsync();
+if (root == null || root.Id == null)
+{
+	Console.WriteLine("OneDrive のルートを取得できませんでした。");
+	return;
+}
+
+// ルートにテスト用のフォルダ作成
+var testFolderItem = await OneDriveUtil.CreateFolder(graphClient, drive.Id, root.Id, testFolderName);
+if (testFolderItem == null || testFolderItem.Id == null)
+{
+	Console.WriteLine("テストフォルダを作成できませんでした。");
+	return;
+}
+
+// テスト用ファイル作成
+if (isCreateTestFile)
+{
+	for (var i = 0;	i < 250; i++)
+	{
+		using MemoryStream ms = new MemoryStream();
+		ms.Write("テストです"u8);
+		ms.Seek(0, SeekOrigin.Begin);
+		var fileName = $"{DateTime.Now:yyyyMMddHHmmss}_{i + 1}.txt";
+		Console.WriteLine($"{fileName} をアップロードしています。");
+		await OneDriveUtil.UploadFile(graphClient, drive.Id, testFolderItem.Id, fileName, ms);
+	}
+}
+
+// テストフォルダの中身一覧取得
+var folderChildren = await graphClient.Drives[drive.Id].Items[testFolderItem.Id].Children.GetAsync();
+if (folderChildren == null || folderChildren.Value == null)
+{
+	Console.WriteLine("フォルダの一覧を取得できませんでした。");
+	return;
+}
+
+Console.WriteLine("Value プロパティを使用した一覧取得です。");
+var count = 0;
+foreach (var item in folderChildren.Value)
+{
+	count++;
+	Console.WriteLine($"{count:d4}: Type={(item.File != null ? "File" : "Folder")}, Id={item.Id}, Name={item.Name}, Size={item.Size}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("PageIterator を使用した一覧取得です。");
+count = 0;
+var pageIterator = PageIterator<DriveItem, DriveItemCollectionResponse>
+		.CreatePageIterator(
+				graphClient,
+				folderChildren,
+				(item) =>
+				{
+					count++;
+					Console.WriteLine($"{count:d4}: Type={(item.File != null ? "File" : "Folder")}, Id={item.Id}, Name={item.Name}, Size={item.Size}");
+					return true;  // false を返すまで次のアイテムを列挙します
+				});
+await pageIterator.IterateAsync();
+
